0b2a26fc95f829b041ec9266b1cad8c78d886a7f
Rajan <rajansahu713@gmail.com>
2024-06-11 22:59:33 +0530

    s3upload removed

--- s3upload/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import FileUpload
-
-# Register your models here.
-
-@admin.register(FileUpload)
-class FileUploadAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in FileUpload._meta.fields]
--- s3upload/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import FileUploadView
-
-urlpatterns = [
-    path('uploads/', FileUploadView.as_view(), name='file_upload'),
-    
-    ]
